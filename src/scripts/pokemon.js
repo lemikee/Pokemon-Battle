211fcba4ad32e5d582efb7b47cb86ef95549729f
Mike Le <letumichael@gmail.com>
2021-09-05 08:27:11 -0700

    Added Pokemon class

---
+++ src/scripts/pokemon.js
@@ -0,0 +1,10 @@
+class Pokemon {
+  constructor(name, sprite, hp, moves) {
+    this.name = name;
+    this.sprite = sprite;
+    this.hp = hp;
+    this.fullhp = hp;
+    this.moves = moves;
+  }
+
+}
